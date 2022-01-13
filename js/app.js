8b577ef4b8351f7d476745ee5c267a8f876864f7
Michael Maker <maker.mdm@gmail.com>
2022-01-13 15:00:54 -0800

    Code lab 04 update q7

--- js/app.js
+++ js/app.js
@@ -125,47 +125,51 @@ guessesClue();
 
 
 //7th Q
-let lastMovie = ['Moana', 'Cabin in the Woods', 'Encanto', 'Dont look up'];
-let movieGuesses = 6;
-let rightAnswer = false;
-/*
-while(movieGuesses){
-  let userMovie = prompt('What\'s the last movie I watched?').toLowerCase();
-
-  for(let i = 0; i < lastMovie.length; i++){
-    if (userMovie === lastMovie[i]){
+function guessMovie(){
+  let lastMovie = ['Moana', 'Cabin in the Woods', 'Encanto', 'Dont look up'];
+  let movieGuesses = 6;
+  let rightAnswer = false;
+  /*
+  while(movieGuesses){
+    let userMovie = prompt('What\'s the last movie I watched?').toLowerCase();
+  
+    for(let i = 0; i < lastMovie.length; i++){
+      if (userMovie === lastMovie[i]){
+        alert(`You guessed right! ${userMovie} was one of the last movies I have watched in 3 months!`);
+        rightAnswer = true;
+        break;
+      }
+    }
+  
+    movieGuesses--;
+  
+  }
+  */
+  
+  for(let j = 0; j < movieGuesses; j++){
+    let userMovie = prompt('What\'s the last movie I watched?').toLowerCase();
+    for(let i = 0; i < lastMovie.length; i++){
+      if(userMovie === lastMovie[i]){
+        rightAnswer = true;
+        break;
+      }
+    }
+    if (rightAnswer){
+      console.log(`You guessed right! ${userMovie} was one of the last movies I have watched in 3 months!`);
       alert(`You guessed right! ${userMovie} was one of the last movies I have watched in 3 months!`);
-      rightAnswer = true;
+      counter++;
       break;
+    } else {
+      alert('Nope, haven\'t seen it.');
     }
   }
-
-  movieGuesses--;
-
-}
-*/
-
-for(let j = 0; j < movieGuesses; j++){
-  let userMovie = prompt('What\'s the last movie I watched?').toLowerCase();
-  for(let i = 0; i < lastMovie.length; i++){
-    if(userMovie === lastMovie[i]){
-      rightAnswer = true;
-      break;
-    }
-  }
-  if (rightAnswer){
-    console.log(`You guessed right! ${userMovie} was one of the last movies I have watched in 3 months!`);
-    alert(`You guessed right! ${userMovie} was one of the last movies I have watched in 3 months!`);
-    counter++;
-    break;
-  } else {
-    alert('Nope, haven\'t seen it.');
+  if(rightAnswer === false){
+    alert(`Correct answers were: ${lastMovie[0]}, ${lastMovie[1]}, ${lastMovie[2]}, ${lastMovie[3]}.`);
   }
 }
-if(rightAnswer === false){
-  alert(`Correct answers were: ${lastMovie[0]}, ${lastMovie[1]}, ${lastMovie[2]}, ${lastMovie[3]}.`);
-}
+guessMovie();
 
+//FINAL ALERT
 alert(`Thanks for playing ${user}! I am a Code Fellow that does not like sweets. I live in a part in California that is not Snowing. This quarantine sometimes makes me feel like I'm in groundhogs day! But that's ok, because I get to learn cool things by interacting through my monitor!`);
 
 alert(`Thanks ${user}! You have answered ${counter} questions correctly.`);
